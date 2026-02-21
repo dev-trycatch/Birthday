1f3808283a95ab31bb62b776f98968cca9d36db1
dev-trycatch <125173253+dev-trycatch@users.noreply.github.com>
2026-02-21 14:17:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
   var confettiContainer = document.getElementById('confetti');
 
   var typewriterLines = [
-    'Wishing you a very Happy Birthday, Mohnish Sir!',
+    'Wishing you a very Happy Birthday, Mohnish Sir!🥳',
     'May this year bring you joy, health and success.',
     'Cheers to many more wonderful years!'
   ];
